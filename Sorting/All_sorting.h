099361f02d022c138f88e43320872e955932cb44
Kaygo <hamchampion@163.com>
2017-11-30 11:52:04 +0800

    Create All_sorting.h

---
+++ Sorting/All_sorting.h
@@ -0,0 +1,14 @@
+//author:KAI
+#ifndef _All_sorting_H_
+#define _All_sorting_H_
+typedef int Elementtype;
+void Bubble_sort(Elementtype A[], int N);//冒泡排序
+void Insert_sort(Elementtype A[], int N);//插入排序
+void Shell_sort(Elementtype A[], int N);//希尔排序
+void Heap_sort(Elementtype A[], int N);//堆排序
+void Merge_sort(Elementtype A[], int N);//归并排序
+void Quick_sort(Elementtype A[], int left,int right);//快速排序
+void Bucket_sort(Elementtype A[], int N);//桶排序
+
+
+#endif
